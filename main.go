77bcb7bbca0b17423059ab5c632c603f87d111f2
Macbook Air <macbookair@MacBook-Air-NIKITA--Macbook.local>
2025-02-19 10:00:27 +0400

    конечная версия

--- main.go
+++ main.go
@@ -32,8 +32,8 @@ type myFile struct {
 	// Weight — размер файла
 	Weight float64 `json:"weight"`
 
-	// Weight_name — название размерности файла
-	Weight_name string `json:"weight_name"`
+	// WeightName — категория размерности файла
+	WeightName string `json:"weight_name"`
 }
 
 type ByWeight []myFile
@@ -44,29 +44,29 @@ func (a ByWeight) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
 func main() {
 	start := time.Now()
-	defer func ()  {
+	defer func() {
 		log.Println("Время работы сервера:", time.Since(start))
 	}()
 
 	err := godotenv.Load()
 
-	if err != nil{
+	if err != nil {
 		log.Fatal("Ошибка чтения .env файла", err)
 	}
 
 	port := os.Getenv("PORT")
 
 	server := &http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
 		Handler: nil,
 	}
 
 	absPath, err := filepath.Abs(".")
-	if err != nil{
+	if err != nil {
 		fmt.Println("Ошибка текущего путя", err)
 	}
 
-	fs := http.FileServer(http.Dir("./static"))
+	fileserver := http.FileServer(http.Dir("./static"))
 
 	http.HandleFunc("/api/fs", handler)
 
@@ -78,50 +78,50 @@ func main() {
 		http.ServeFile(w, r, "./static/index.html")
 	})
 
-	http.Handle("/static/", http.StripPrefix("/static", fs))
+	http.Handle("/static/", http.StripPrefix("/static", fileserver))
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	
+
 	go func() {
 		log.Println("Starting server on port", port)
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed{
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatal("Error starting server...", err)
 		}
 	}()
 
-	sig :=  <- sigChan
+	sig := <-sigChan
 
 	log.Println("Получен сигнал, начинаем завершение работы", sig)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	if err := server.Shutdown(ctx);err != nil{
+	if err := server.Shutdown(ctx); err != nil {
 		log.Fatalf("Ошибка при завершении работы сервера %v", err)
 	}
 }
 
-//	getSize возвращает размер файла, директории
+// getSize возвращает размер файла, директории
 func getSize(path string) int64 {
 
 	var size int64
 	files, err := os.ReadDir(path)
 
-	if err != nil{
+	if err != nil {
 		fmt.Println("Ошибка при чтении директории", err)
 		return 0
 	}
 
-	for _, file := range files{
+	for _, file := range files {
 
 		file, err := file.Info()
 
-		if err != nil{
+		if err != nil {
 			fmt.Println("Ошибка информации о файле", err)
 			continue
 		}
 		size += file.Size()
-		if file.IsDir(){
+		if file.IsDir() {
 			size += getSize(path + "/" + file.Name())
 		}
 	}
@@ -133,19 +133,19 @@ func checkPath(path string) (string, error) {
 
 	var err error
 
-	if strings.HasPrefix(path, "~"){
+	if strings.HasPrefix(path, "~") {
 
 		home_path, err := os.UserHomeDir()
 
-		if err != nil{
+		if err != nil {
 			return "", fmt.Errorf("Ошибка путя с префиксом %s %w", home_path, err)
 		}
 		path = strings.Replace(path, "~", home_path, 1)
 
 		fmt.Println(path)
-	}else{
+	} else {
 		path, err = filepath.Abs(os.ExpandEnv(path))
-		if err != nil{
+		if err != nil {
 			return "", fmt.Errorf("Ошибка относительно директории %s %w", path, err)
 		}
 	}
@@ -161,21 +161,21 @@ func convertBytes(bytes float64) (float64, string) {
 	)
 
 	if bytes >= GB {
-		return math.Round(bytes / GB * 10) / 10, "GB"
+		return math.Round(bytes/GB*10) / 10, "GB"
 	} else if bytes >= MB {
-		return math.Round(bytes / MB * 10) / 10, "MB"
+		return math.Round(bytes/MB*10) / 10, "MB"
 	} else if bytes >= KB {
-		return math.Round(bytes / KB * 10) / 10, "KB"
+		return math.Round(bytes/KB*10) / 10, "KB"
 	}
 	return bytes, "Bytes"
 }
 
 // handler считываем запрос, возвращаем данные в формате json
-func handler(w http.ResponseWriter, r *http.Request)  {
+func handler(w http.ResponseWriter, r *http.Request) {
 	const dirSize = 4096
 
 	log.Println(r.Method, r.URL)
-	if r.Method == "GET"{
+	if r.Method == "GET" {
 		query := r.URL.Query()
 
 		w.Header().Set("Content-Type", "application/json")
@@ -183,60 +183,60 @@ func handler(w http.ResponseWriter, r *http.Request)  {
 		path := query.Get("root")
 		type_sort := query.Get("sort")
 
-		if len(query) == 0{
+		if len(query) == 0 {
 			path = "./"
 			type_sort = "desc"
 		}
-		if type_sort == ""{
+		if type_sort == "" {
 			type_sort = "desc"
 		}
 
 		expandedPath, err := checkPath(path)
 
-		if err != nil{
+		if err != nil {
 			http.Error(w, "Ошибка проверки пути", http.StatusBadRequest)
 			return
 		}
 
 		files, err := os.ReadDir(expandedPath)
-		if err != nil{
+		if err != nil {
 			http.Error(w, "Ошибка при чтении директории", http.StatusBadRequest)
 			return
 		}
-	
+
 		wg := sync.WaitGroup{}
 
 		array := make([]myFile, len(files))
 
-		for idx, file := range files{
+		for idx, file := range files {
 
 			finfo, err := file.Info()
 
-			if err != nil{
+			if err != nil {
 				fmt.Println("Ошибка информации о файле", err)
 				continue
 			}
 			wg.Add(1)
 			go func(fileinfo fs.FileInfo, c int) {
 				defer wg.Done()
-				if fileinfo.IsDir(){
-					size := getSize(expandedPath + "/" + fileinfo.Name()) + fileinfo.Size() - dirSize
+				if fileinfo.IsDir() {
+					size := getSize(expandedPath+"/"+fileinfo.Name()) + fileinfo.Size() - dirSize
 
 					array[c] = myFile{"d", fileinfo.Name(), float64(size), "Bytes"}
-				}else{
+				} else {
 					array[c] = myFile{"f", fileinfo.Name(), float64(fileinfo.Size()), "Bytes"}
 				}
 			}(finfo, idx)
 
 		}
 		wg.Wait()
-		
-		if err := sortFiles(type_sort, array); err != nil{
-			http.Error(w, "Ошибка выбора сортировки: " + err.Error(), http.StatusBadRequest)
+
+		if err := sortFiles(type_sort, array); err != nil {
+			http.Error(w, "Ошибка выбора сортировки: "+err.Error(), http.StatusBadRequest)
 			return
 		}
 
-		if err := json.NewEncoder(w).Encode(array); err != nil{
+		if err := json.NewEncoder(w).Encode(array); err != nil {
 			http.Error(w, "Ошибка кодировки json", http.StatusInternalServerError)
 		}
 	}
@@ -258,7 +258,7 @@ func sortFiles(type_sort string, array []myFile) error {
 	}
 
 	for i := range array {
-		array[i].Weight, array[i].Weight_name = convertBytes(array[i].Weight)
+		array[i].Weight, array[i].WeightName = convertBytes(array[i].Weight)
 	}
 
 	return nil
