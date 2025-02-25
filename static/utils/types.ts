2dfdb23290125c730f21e2c3731b6c3d4de5c332
Macbook Air <macbookair@MacBook-Air-NIKITA--Macbook.local>
2025-02-25 18:28:53 +0400

    Сделал декомпозицию typescript

---
+++ static/utils/types.ts
@@ -0,0 +1,7 @@
+// src/types.ts
+export interface MyFile {
+    category: string;
+    name: string;
+    weight: number;
+    weight_name: string;
+}
---
+++ static/utils/updateTable.ts
@@ -0,0 +1,43 @@
+// src/updateTable.ts
+import { fetchFiles } from "./api/fileService";
+import { renderTable } from "./components/fileGrid";
+import { showLoader, hideLoader } from "./components/loader";
+import { getRootFromUrl, getSortFromUrl, updateUrl } from "./utils/urlHelper";
+import { MyFile } from "./types";
+
+let controller: AbortController | null = null;
+
+export function updateTable(): void {
+    if (controller) {
+        controller.abort();
+    }
+    controller = new AbortController();
+    const signal = controller.signal;
+
+    const loader = document.getElementById("loader");
+    const fileGrid = document.querySelector(".file-grid") as HTMLElement | null;
+
+    let root = getRootFromUrl();
+    let sort = getSortFromUrl();
+
+    const buttons = document.querySelectorAll(".button");
+    buttons.forEach((button) => (button as HTMLButtonElement).disabled = true);
+
+    showLoader(loader);
+    if (fileGrid) fileGrid.classList.add("hidden");
+
+    fetchFiles(root, sort, signal)
+        .then((data: MyFile[]) => {
+            renderTable(data, root);
+        })
+        .catch((error: Error) => {
+            if (error.name !== "AbortError") {
+                console.error("Ошибка в запросе", error);
+            }
+        })
+        .finally(() => {
+            buttons.forEach((button) => (button as HTMLButtonElement).disabled = false);
+            hideLoader(loader);
+            if (fileGrid) fileGrid.classList.remove("hidden");
+        });
+}
